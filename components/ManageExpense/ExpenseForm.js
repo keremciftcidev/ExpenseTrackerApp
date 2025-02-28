aa0c5ab8d04be056791844b07e94a90abe835dd3
Kerem Ciftci <krmciftci17@gmail.com>
2025-03-01 01:37:00 +0300

    Setting the Form Layout

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,26 +1,61 @@
-import { View } from "react-native";
+import { View ,StyleSheet,Text} from "react-native";
 import Input from "./Input";
+import { GlobalStyles } from "../../constants/styles";
 
 function ExpenseForm() {
-    function amountChangeHandler(){
+  function amountChangedHandler() {}
 
-    }
-
-    return <View>
-        <Input label="Amount" textInputConfig={{
-            keyboardType:"Decimal-pad",
-            onChangeText:amountChangeHandler
-        }}/>
-        <Input label="Date" textInputConfig={{
-            placeholder:"YYYY-MM-DD",
-            maxLength:10,
-            onChangeText:()=>{}
-        }}/>
-        <Input label="Description" textInputConfig={{
-            multiline:true,
-            //autoCapitalize:"none"
-        }}/>
+  return (
+    <View style={styles.formStyle}>
+      <Text style={styles.inputText}>Your Expense</Text>
+      <View style={styles.inputRows}>
+        <Input
+        style={styles.rowInput}
+          label="Amount"
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: amountChangedHandler,
+          }}
+        />
+        <Input
+         style={styles.rowInput}
+          label="Date"
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: () => {},
+          }}
+        />
+      </View>
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+          //autoCapitalize:"none"
+        }}
+      />
     </View>
+  );
 }
 
 export default ExpenseForm;
+
+const styles = StyleSheet.create({
+  formStyle:{
+    marginTop:40
+  },
+  inputText:{
+    color:"white",
+    fontSize:24,
+    marginVertical:24,
+    fontWeight:"bold",
+    textAlign:"center"
+  },
+  inputRows:{
+    flexDirection:"row",
+    justifyContent:"space-between"
+  },
+  rowInput:{
+    flex:1
+  }
+})
--- store/expenses-contect.js
+++ store/expenses-contect.js
@@ -5,36 +5,36 @@ const DUMMY_EXPENSES = [
         id:"e1",
         description:"A pair of shoes",
         amount:59.99,
-        date: new Date("2025-02-10")
+        date: new Date("2025-02-28")
     },
     {
         id:"e2",
         description:"A pair of trousers",
         amount:109.99,
-        date: new Date("2025-02-19")
+        date: new Date("2025-02-27")
     },{
         id:"e3",
         description:"Some bananas ",
         amount:5.99,
-        date: new Date("2025-02-19")
+        date: new Date("2025-02-26")
     },
     {
         id:"e4",
         description:"book",
         amount:14.99,
-        date: new Date("2025-02-19")
+        date: new Date("2025-02-25")
     },
     {
       id:"e5",
       description:"Some bananas ",
       amount:5.99,
-      date: new Date("2025-02-18")
+      date: new Date("2025-02-24")
   },
   {
       id:"e6",
       description:"shoes",
       amount:159.99,
-      date: new Date("2025-02-19")
+      date: new Date("2025-02-23")
   }
 ]
 
